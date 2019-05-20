cfdfefde8e5a34d32ae6a232851c86fef5b33d4d
Gabriel Kabbe <gabriel.kabbe@mail.de>
2019-05-20 22:49:32 +0200

    Update bees

--- abc_algorithm/main.py
+++ abc_algorithm/main.py
@@ -7,13 +7,26 @@ logger = logging.getLogger(__name__)
 
 
 class Bee:
-    def __init__(self, pos, *, func, limit, search_radius=1):
+    def __init__(self, pos, *, func, dim, limit, search_radius=1, lower_bound, upper_bound):
         self.pos = np.atleast_1d(pos)
         self.func = func
+        self.dim = dim
         self.limit = limit
         self.search_radius = search_radius
         self._fitness = None
         self.tries = 0
+        self.bounds = (lower_bound, upper_bound)
+
+    def global_update(self):
+        self.pos = np.random.uniform(lower_bound, upper_bound, dim)
+
+    @classmethod
+    def random_init(cls, *, func, dim, limit, search_radius=1, lower_bound=-100,
+                    upper_bound=100):
+        bee = cls(pos, func=func, dim=dim, limit=limit, search_radius=search_radius,
+                  lower_bound=lower_bound, upper_bound=upper_bound)
+        bee.global_update()
+        return bee
 
     def calculate_fitness(self, pos):
         fx = self.func(pos)
@@ -33,8 +46,11 @@ class EmployedBee(Bee):
         logger.debug("Doing local update")
         random_pos = np.random.choice(positions)
         logger.debug("Testing new position %s", random_pos)
-        new_pos = (self.pos +
-            np.random.uniform(-self.search_radius, self.search_radius, 2) * (random_pos - self.pos))
+        i = np.random.randint(0, self.dim)
+        new_pos_i = (self.pos[i] +
+            np.random.uniform(-self.search_radius, self.search_radius, 2) * (random_pos[i] - self.pos[i]))
+        new_pos = np.copy(self.pos)
+        new_pos[i] = new_pos_i
         old_fitness = self._fitness
         logger.debug("Old fitness value: %.2e", old_fitness)
         new_fitness = self.calculate_fitness(new_pos)
@@ -48,4 +64,13 @@ class EmployedBee(Bee):
 
 
 class OnlookerBee(EmployedBee):
+    def choose_food_source(self, employed_bees: list):
+        fitness_vals = [bee.fitness for bee in employed_bees]
+        cumsum = np.cumsum(fitness_vals)
+        idx = np.searchsorted(cumsum, np.random.uniform(0, cumsum[-1]))
+        self.pos = employed_bees[idx].pos
+
+
+class Scout(Bee):
     pass
+
